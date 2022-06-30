9402b79b38964ca8578db2ad5d58dfb2af184c83
Pierce <P.Hahn94@outlook.com>
2022-06-30 18:26:54 -0300

    changes to image map urls

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,111 +1,100 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore';
 import GridLoader from 'react-spinners/ClipLoader';
 import '../styles/Main.css';
 import leaderBoard from '../images/leaderBoard.png';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAM4WXJajjCg7dDSakwcNr5bi6hO9_Q6Ck',
-  authDomain: 'where-is-waldo-game.firebaseapp.com',
-  projectId: 'where-is-waldo-game',
-  storageBucket: 'where-is-waldo-game.appspot.com',
-  messagingSenderId: '214090924447',
-  appId: '1:214090924447:web:5cb4b77a2da741f10cfdaa',
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
 function Main() {
-  const [imageOne, setImageOne] = useState();
-  const [imageTwo, setImageTwo] = useState();
-  const [imageThree, setImageThree] = useState();
   const [loading, setLoading] = useState(true);
-
-  async function getImages() {
-    const colRef = collection(db, 'selectionImages');
-    const mapDocs = await getDocs(colRef);
-
-    const mapUrlAddressOne = await mapDocs.docs[0].data().URL;
-    const mapUrlAddressTwo = await mapDocs.docs[1].data().URL;
-    const mapUrlAddressThree = await mapDocs.docs[2].data().URL;
-
-    const urlOne = `url("${mapUrlAddressOne}")`;
-    const urlTwo = `url("${mapUrlAddressTwo}")`;
-    const urlThree = `url("${mapUrlAddressThree}")`;
-
-    setImageOne({
-      backgroundImage: urlOne,
-    });
-    setImageTwo({
-      backgroundImage: urlTwo,
-    });
-    setImageThree({
-      backgroundImage: urlThree,
-    });
-
-    setLoading(false);
-  }
+  const [imageOne, setImageOne] = useState(false);
+  const [imageTwo, setImageTwo] = useState(false);
+  const [imageThree, setImageThree] = useState(false);
 
   useEffect(() => {
-    getImages();
-  }, []);
+    console.log('fire');
+    if (imageOne === true && imageTwo === true && imageThree === true) {
+      setLoading(false);
+      console.log('fire');
+    }
+  }, [imageOne, imageTwo, imageThree]);
 
   return (
     <div className="Main">
       <div className="mainContainer">
         <div className="mainLoadingContainer">
-          {loading ? (
-            <div className="loader">
-              <GridLoader
-                color="red"
-                loading={loading}
-                size={150}
-                speedMultiplier={1}
-              />
-            </div>
-          ) : (
-            <Link to="/GameOne" className="linkContainer">
-              <p className="link easyMap">Easy</p>
-              <div style={imageOne} className="mainLoadingImage" />
-            </Link>
-          )}
+          <div className={`loader ${loading ? 'showLoader' : 'hideLoader'}`}>
+            <GridLoader
+              color="red"
+              loading={loading}
+              size={150}
+              speedMultiplier={1}
+            />
+          </div>
+          <Link
+            to="/GameOne"
+            className={`linkContainer ${
+              loading ? 'hideLinkContent' : 'showLinkContent'
+            }`}
+          >
+            <p className="link easyMap">Easy</p>
+            <img
+              className="mainLoadingImage"
+              src="https://firebasestorage.googleapis.com/v0/b/where-is-waldo-game.appspot.com/o/selectionOne.jpg?alt=media&token=18ba5ff6-5a7a-4640-8696-dfbd9dfc7a84"
+              alt="map one"
+              onLoad={() => {
+                setImageOne(true);
+                console.log('loaded');
+              }}
+            />
+          </Link>
         </div>
         <div className="mainLoadingContainer">
-          {loading ? (
-            <div className="loader">
-              <GridLoader
-                color="red"
-                loading={loading}
-                size={150}
-                speedMultiplier={1}
-              />
-            </div>
-          ) : (
-            <Link to="/GameTwo" className="linkContainer">
-              <p className="link hardMap">Hard</p>
-              <div style={imageTwo} className="mainLoadingImage" />
-            </Link>
-          )}
+          <div className={`loader ${loading ? 'showLoader' : 'hideLoader'}`}>
+            <GridLoader
+              color="red"
+              loading={loading}
+              size={150}
+              speedMultiplier={1}
+            />
+          </div>
+          <Link
+            to="/GameTwo"
+            className={`linkContainer ${
+              loading ? 'hideLinkContent' : 'showLinkContent'
+            }`}
+          >
+            <p className="link hardMap">Hard</p>
+            <img
+              className="mainLoadingImage"
+              src="https://firebasestorage.googleapis.com/v0/b/where-is-waldo-game.appspot.com/o/selectionTwo.jpg?alt=media&token=733357aa-ff97-476b-a22b-cba20cb656e4"
+              alt="map two"
+              onLoad={() => setImageTwo(true)}
+            />
+          </Link>
         </div>
         <div className="mainLoadingContainer">
-          {loading ? (
-            <div className="loader">
-              <GridLoader
-                color="red"
-                loading={loading}
-                size={150}
-                speedMultiplier={1}
-              />
-            </div>
-          ) : (
-            <Link to="/GameThree" className="linkContainer">
-              <p className="link mediumMap">Medium</p>
-              <div style={imageThree} className="mainLoadingImage" />
-            </Link>
-          )}
+          <div className={`loader ${loading ? 'showLoader' : 'hideLoader'}`}>
+            <GridLoader
+              color="red"
+              loading={loading}
+              size={150}
+              speedMultiplier={1}
+            />
+          </div>
+          <Link
+            to="/GameThree"
+            className={`linkContainer ${
+              loading ? 'hideLinkContent' : 'showLinkContent'
+            }`}
+          >
+            <p className="link mediumMap">Medium</p>
+            <img
+              className="mainLoadingImage"
+              src="https://firebasestorage.googleapis.com/v0/b/where-is-waldo-game.appspot.com/o/selectionThree.jpeg?alt=media&token=e40b259c-f388-44c2-8228-5bab63262c2a"
+              alt="map three"
+              onLoad={() => setImageThree(true)}
+            />
+          </Link>
         </div>
       </div>
       <Link to="/LeaderBoard" className="linkMain">
